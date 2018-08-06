4a3621426f34c3f019f1dd5717529d8559a69f37
Rivaldo Guimarães de Lima Junior <rivaldottibd@gmail.com>
2018-08-06 12:29:39 -0300

    First Commit of the API

---
+++ src/test/java/com/rivaldo/pontointeligente/api/PontoIntelienteApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rivaldo.pontointeligente.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PontoIntelienteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
